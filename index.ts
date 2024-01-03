c5f6c3d88827b755d81ba684c7b8b62c08a7f436
Bedirhan SAMSA <bedirhan.samsa@manim.com.tr>
2024-01-03 15:07:21 +0300

    add body to output

--- index.ts
+++ index.ts
@@ -1,62 +1,66 @@
-import * as core from '@actions/core'
-import * as github from '@actions/github'
-import { components } from '@octokit/openapi-types/types'
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+import { components } from "@octokit/openapi-types/types";
 
 async function run(): Promise<void> {
-    // Get input values
-    const myToken = core.getInput('myToken');
-    const excludeReleaseTypes = core.getInput('exclude_types').split('|');
-    const topList = +core.getInput('view_top');
+  // Get input values
+  const myToken = core.getInput("myToken");
+  const excludeReleaseTypes = core.getInput("exclude_types").split("|");
+  const topList = +core.getInput("view_top");
 
-    // Set parameters
-    const excludeDraft = excludeReleaseTypes.some(f => f === "draft");
-    const excludePrerelease = excludeReleaseTypes.some(f => f === "prerelease");
-    const excludeRelease = excludeReleaseTypes.some(f => f === "release");
+  // Set parameters
+  const excludeDraft = excludeReleaseTypes.some((f) => f === "draft");
+  const excludePrerelease = excludeReleaseTypes.some((f) => f === "prerelease");
+  const excludeRelease = excludeReleaseTypes.some((f) => f === "release");
 
-    const octokit = github.getOctokit(myToken);
+  const octokit = github.getOctokit(myToken);
 
-    // Load release list from GitHub
-    let releaseList = await octokit.rest.repos.listReleases({
-        repo: github.context.repo.repo,
-        owner: github.context.repo.owner,
-        per_page: topList,
-        page: 1
-    });
+  // Load release list from GitHub
+  let releaseList = await octokit.rest.repos.listReleases({
+    repo: github.context.repo.repo,
+    owner: github.context.repo.owner,
+    per_page: topList,
+    page: 1,
+  });
 
-    // Search release list for latest required release
-    if (core.isDebug()) {
-        core.debug(`Found ${releaseList.data.length} releases`);
-        releaseList.data.forEach((el) => WriteDebug(el));
+  // Search release list for latest required release
+  if (core.isDebug()) {
+    core.debug(`Found ${releaseList.data.length} releases`);
+    releaseList.data.forEach((el) => WriteDebug(el));
+  }
+
+  for (let i = 0; i < releaseList.data.length; i++) {
+    let releaseListElement = releaseList.data[i];
+
+    if (
+      (!excludeDraft && releaseListElement.draft) ||
+      (!excludePrerelease && releaseListElement.prerelease) ||
+      (!excludeRelease &&
+        !releaseListElement.prerelease &&
+        !releaseListElement.draft)
+    ) {
+      core.debug(`Chosen: ${releaseListElement.id}`);
+      setOutput(releaseListElement);
+      break;
     }
-
-    for (let i = 0; i < releaseList.data.length; i++) {
-        let releaseListElement = releaseList.data[i];
-
-        if ((!excludeDraft && releaseListElement.draft) ||
-            (!excludePrerelease && releaseListElement.prerelease) ||
-            (!excludeRelease && !releaseListElement.prerelease && !releaseListElement.draft)) {
-            core.debug(`Chosen: ${releaseListElement.id}`);
-            setOutput(releaseListElement);
-            break;
-        }
-    }
+  }
 }
-
 
 /**
  * Setup action output values
  * @param release - founded release
  */
 function setOutput(release: components["schemas"]["release"]): void {
-    core.setOutput('id', release.id);
-    core.setOutput('name', release.name);
-    core.setOutput('tag_name', release.tag_name);
-    core.setOutput('created_at', release.created_at);
-    core.setOutput('draft', release.draft);
-    core.setOutput('prerelease', release.prerelease);
-    core.setOutput('release', !release.prerelease && !release.draft);
-    core.setOutput('url', release.url);
-    core.setOutput('html_url', release.html_url);
+  core.setOutput("id", release.id);
+  core.setOutput("name", release.name);
+  core.setOutput("tag_name", release.tag_name);
+  core.setOutput("created_at", release.created_at);
+  core.setOutput("draft", release.draft);
+  core.setOutput("prerelease", release.prerelease);
+  core.setOutput("release", !release.prerelease && !release.draft);
+  core.setOutput("url", release.url);
+  core.setOutput("html_url", release.html_url);
+  core.setOutput("body", release.body);
 }
 
 /**
@@ -64,14 +68,15 @@ function setOutput(release: components["schemas"]["release"]): void {
  * @param release - founded release
  */
 function WriteDebug(release: components["schemas"]["release"]): void {
-    core.debug(`id: ${release.id}`);
-    core.debug(`name: ${release.name}`)
-    core.debug(`tag_name: ${release.tag_name}`);
-    core.debug(`created_at: ${release.created_at}`);
-    core.debug(`draft: ${release.draft}`);
-    core.debug(`prerelease: ${release.prerelease}`);
-    core.debug(`url: ${release.url}`)
-    core.debug(`html_url: ${release.html_url}`)
+  core.debug(`id: ${release.id}`);
+  core.debug(`name: ${release.name}`);
+  core.debug(`tag_name: ${release.tag_name}`);
+  core.debug(`created_at: ${release.created_at}`);
+  core.debug(`draft: ${release.draft}`);
+  core.debug(`prerelease: ${release.prerelease}`);
+  core.debug(`url: ${release.url}`);
+  core.debug(`html_url: ${release.html_url}`);
+  core.debug(`body: ${release.body}`);
 }
 
 run();
